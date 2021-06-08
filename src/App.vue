8dba6f992137379ed63935a0624d648ca4bb9d4d
jjj <lovealohajjj@gmail.com>
2021-06-09 00:28:46 +0900

    Todo List

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    />
+    <hr>
+    <Todo
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo"
+    />
+    {{todos}}
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Todo from '@/components/Todo.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: 'buy a car', checked: false },
+        { id: 2, text: 'buy a pants', checked: false },
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      console.log(e.target.value)
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      })
+      this.todoText = ''
+    },
+    toggleCheckbox({id, checked}) {
+      console.log(id, checked)
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      })
+      this.todos[index].checked = checked
+    },
+    deleteTodo(id) {
+      // case 01
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id === id
+      // })
+      // this.todos.splice(index, 1)
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+      // case 02
+      this.todos = this.todos.filter(todo => todo.id !== id)
     }
   }
 }
+</script>
+
+
+<style lang="scss">
 </style>
