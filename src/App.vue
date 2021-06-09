ae67c9b95c794ceabb098edd130ea0e50b0956f8
jjj <lovealohajjj@gmail.com>
2021-06-09 23:29:05 +0900

    vuex02 - state

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <CompletedTodo
-      :todos="todos"
-    />
+    <CompletedTodo />
     <AddTodo
       @add-todo="addTodo"
     />
     <hr>
     <TodoList
-      :todos="todos"
       @toggle-checkbox="toggleCheckbox"
       @click-delete="deleteTodo"
     />
@@ -30,10 +27,6 @@ export default {
   data() {
     return {
       todoText: '',
-      todos: [
-        { id: 1, text: 'buy a car', checked: false },
-        { id: 2, text: 'buy a pants', checked: false },
-      ]
     }
   },
   methods: {
