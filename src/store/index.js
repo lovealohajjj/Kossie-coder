e855385e9efb73746f4b510cf699056a5d1faa4f
정진주 <mz_pearl@woowafriends.com>
2021-06-12 19:30:19 +0900

    오타 수정

--- src/store/index.js
+++ src/store/index.js
@@ -29,7 +29,7 @@ export default new Vuex.Store({
       })
       state.todos[index].checked = checked
     },
-    DELELTE_TODO(state, todoId) {
+    DELETE_TODO(state, todoId) {
       const index = state.todos.findIndex(todo => {
         return todo.id === todoId
       })
